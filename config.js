446b1c1b559689fe02fc62039078dd9e986aa2e7
Oscar <osanjuand1300@alumno.ipn.mx>
2020-12-10 10:29:29 -0600

    cambio

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
  var host = 'https://adam.anahuac.mx/appis-serviciosocial/api';
  //var host = 'https://serviciosocial.gesdesapplication.com/api';
 
- var empresasurl = 'https://adam.anahuac.mx/serviciosocial/empresas/';
+ var empresasurl = 'https://adam.anahuac.mx/serviciosocial/instituciones/';
  var alumnosurl = 'https://adam.anahuac.mx/serviciosocial/alumnos/';
 
  // var empresasurl = 'https://portal-ss.gesdesapplication.com/alumnos/';
