43e81945c0579a403b3f8b6d6f5a4d25148f1bb9
Gesdes <50413650+Gesdes@users.noreply.github.com>
2021-01-17 18:05:56 -0600

    admin

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
  var host = 'https://adam.anahuac.mx/appis-serviciosocial/api';
- //var host = 'https://serviciosocial.gesdesapplication.com/api';
+//  var host = 'https://serviciosocial.gesdesapplication.com/api';
 
  var empresasurl = 'https://adam.anahuac.mx/serviciosocial/instituciones/';
  var alumnosurl = 'https://adam.anahuac.mx/serviciosocial/alumnos/';
 
- // var empresasurl = 'https://portal-ss.gesdesapplication.com/alumnos/';
- // var alumnosurl = 'https://portal-ss.gesdesapplication.com/alumnos/';
+//  var empresasurl = 'https://portal-ss.gesdesapplication.com/alumnos/';
+//  var alumnosurl = 'https://portal-ss.gesdesapplication.com/alumnos/';
